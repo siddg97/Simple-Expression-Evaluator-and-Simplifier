e36d40b3cff02639a23f22cc84946ff2a1ae98bf
Siddharth Gupta <g.sidd97@gmail.com>
2019-06-11 03:24:24 -0700

    Update myeval.scm

--- myeval.scm
+++ myeval.scm
@@ -1,24 +1,15 @@
 ;;; <---- START : myeval.scm  ----->
 
-;;;+------------------------------------------------------------------------------------------------------------+
-;;;| ASSIGNMENT 2 - CMPT 383 - SUMMER 2019 																		|
-;;;| Instructor: Toby Donaldson																					|
-;;;| Author: Siddharth Gupta | SFU ID: 301327469																|
-;;;| Due-Date: June 5 2019																						|
-;;;|																											|
-;;;| IMPEMENTATION:																								|
-;;;| 	+ Created several top-level checking functions to check a given expression 								|
-;;;|	+ Called them recursively in other functions.															|
-;;;| 	+ (myeval expr env) provides certain error messages for certain cases									|
-;;;|																											| 
-;;;| CITATIONS:																									|
-;;;|	- Assignment 2 description 	[ http://www.sfu.ca/~tjd/383summer2019/a2.html ]							|
-;;;|	- Course notes for Scheme on course home page [ http://www.sfu.ca/~tjd/383summer2019/index.html ]		|
-;;;|																											|
-;;;| NOTE:																										|
-;;;| 	- I am loading my "env1.scm" file for using in (myeval expr env)										|
-;;;|	- I am loading the module "helpful_stuff.scm" which has helpful functions for all assignment questions	|
-;;;+------------------------------------------------------------------------------------------------------------+
+;;;+------------------------------------------------------------------------------------+
+;;;| IMPEMENTATION:									|
+;;;| 	+ Created several top-level checking functions to check a given expression 	|
+;;;|	+ Called them recursively in other functions.					|
+;;;| 	+ (myeval expr env) provides certain error messages for certain cases		|
+;;;|											|
+;;;| NOTE:										|
+;;;| 	- I am loading my "env1.scm" file for using in (myeval expr env)		|
+;;;|	- I am loading the module "helpful_stuff.scm" which has helpful functions 	|
+;;;+------------------------------------------------------------------------------------+
 
 (load "env1.scm")
 (load "helpful_stuff.scm")
